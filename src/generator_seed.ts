10879114c48e0d47c62f7df7379e261f94a948af
Hironori Wada <hironori104@gmail.com>
2022-08-12 01:22:17 +0900

    wip

--- src/generator_seed.ts
+++ src/generator_seed.ts
@@ -1,113 +1,6 @@
 import chalk from "chalk";
 import chroma from "chroma-js";
-import { SeedConfig, Theme } from "./types";
-
-export function generateFromSeed(seed: any, name: string): Theme {
-  return {
-    name: `ergonomic-${name}`,
-    type: "dark",
-    semanticHighlighting: true,
-    semanticTokenColors: {
-      // types
-      class: {
-        fontStyle: "bold",
-        foreground: seed.class,
-      },
-      type: {
-        fontStyle: "bold",
-        foreground: seed.class,
-      },
-      interface: {
-        fontStyle: "bold",
-        foreground: seed.class,
-      },
-      enum: {
-        fontStyle: "bold",
-        foreground: seed.class,
-      },
-      enumMember: seed.variables,
-      typeParameter: {
-        fontStyle: "bold",
-        foreground: seed.class,
-      },
-      namespace: {
-        fontStyle: "bold",
-        foreground: seed.class,
-      },
-      // functions
-      function: {
-        fontStyle: "bold",
-        foreground: seed.function,
-      },
-      method: {
-        fontStyle: "bold",
-        foreground: seed.function,
-      },
-      // variables
-      variable: seed.variables,
-      parameter: seed.parameter,
-      property: seed.property,
-      "property.annotation": seed.keywords,
-      "parameter.label": seed["parameter.label"],
-      // NO WORKING
-      comment: seed.comment,
-      string: seed.string,
-      operator: seed.operators,
-      keyword: seed.keywords,
-      stringLiteral: seed.string,
-      struct: {
-        fontStyle: "bold",
-        foreground: seed.class,
-      },
-      macro: {
-        fontStyle: "italic bold",
-        foreground: seed.function,
-      },
-      newOperator: seed.keywords,
-      numberLiteral: seed.number,
-      number: seed.number,
-      customLiteral: "#ff0000",
-      decorator: "#ff0000",
-      event: "#ff0000",
-      genericType: "#ff0000",
-      label: "#ff0000",
-      memberOperatorOverload: "#ff0000",
-      operatorOverload: "#ff0000",
-      referenceType: "#ff0000",
-      regexp: "#ff0000",
-      templateFunction: "#ff0000",
-      templateType: "#ff0000",
-      valueType: "#ff0000",
-      cliProperty: "#ff0000",
-      // =============================================
-      // =============================================
-      "*.defaultLibrary": {
-        // fontStyle: "italic bold underline",
-        fontStyle: "italic bold",
-      },
-      "variable.defaultLibrary": {
-        // fontStyle: "italic bold underline",
-        fontStyle: "italic bold",
-        foreground: seed.primitiveType,
-      },
-      "*.global": {
-        // fontStyle: "italic bold underline",
-        fontStyle: "italic bold",
-      },
-      "*.static": {
-        fontStyle: "italic bold",
-      },
-      "*.modification": {},
-      "*.readonly": {},
-      "*.abstract": {},
-      "*.async": {},
-      "*.declaration": {},
-      "*.deprecated": {},
-      "*.documentation": {},
-      "*.local": {},
-    },
-  } as any;
-}
+import { Cl } from "./types";
 
 // '#ffc0cb', '#fa72bb', '#cb17d2', '#0000ff'
 
@@ -121,7 +14,30 @@ const seed = {
   operators: "#FFFFFF",
   // primitiveValueBase:
 };
+const ansiKeys = [
+  "red",
+  "green",
+  "yellow",
+  "blue",
+  "purple",
+  "cyan",
+  "white",
+  "black",
+  "redDark",
+  "greenDark",
+  "yellowDark",
+  "blueDark",
+  "purpleDark",
+  "cyanDark",
+  "whiteDark",
+  "blackDark",
+] as const;
+type AnsiKey = typeof ansiKeys[number];
+type AnsiSeed = Record<AnsiKey, Cl>;
 
+function fromAnsi(seed: AnsiSeed) {
+  
+}
 
 async function main(m: chroma.InterpolationMode) {
   const [prop, param, vari] = chroma
@@ -138,7 +54,7 @@ async function main(m: chroma.InterpolationMode) {
   const fs = [_propF, _paramF, _variF];
   const [propF, paramF, variF] = fs.map((c, i) => chroma.mix(c, vs[i], 0.5, m));
 
-  printC({ _propF, _paramF, _variF });
+  printC({ _propF: _propF, _paramF, _variF });
   printC({ propF, paramF, variF });
 
   /* const [propF, paramF, variF] = [prop, param, vari].map((c) =>
@@ -181,7 +97,7 @@ function printC(colors: Record<string, chroma.Color>) {
   );
 }
 
-console.log('rgb');
-main('rgb').catch(console.error);
-console.log('lch');
-main('lch').catch(console.error);
+console.log("rgb");
+main("rgb").catch(console.error);
+console.log("lch");
+main("lch").catch(console.error);
